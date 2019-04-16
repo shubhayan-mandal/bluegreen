b0a737cc5d85755477239617154e9d2afd5f06e7
shubhayan-mandal <chaalyaar@gmail.com>
2019-04-16 12:58:41 -0400

    token commit

--- image.php
+++ image.php
@@ -3,7 +3,6 @@
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 //Line reserved to trigger the webhook in the lab.
-
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'blue') {
